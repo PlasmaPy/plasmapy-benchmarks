6c7b711aae2d02a1a6c581b6ce4ed92cd898a526
Dominik Stańczak <stanczakdominik@gmail.com>
2020-05-06 15:52:32 +0200

    bugfixes

--- benchmarks/benchmark_PhysicsSuite.py
+++ benchmarks/benchmark_PhysicsSuite.py
@@ -6,6 +6,24 @@ import astropy.units as u
 from numpy import pi
 
 
+class mathematics:
+    """
+    Benchmark that times the performance of funcions from
+    mathematics/mathematics.py file
+    """
+
+    def setup(self):
+        pass
+
+    def time_plasma_dispersion_func(self):
+        plasma_dispersion_func(-1.52+0.47j)
+
+    def time_plasma_dispersion_func_deriv(self):
+        plasma_dispersion_func_deriv(-1.52+0.47j)
+
+    def time_Fermi_integral(self):
+        Fermi_integral(1, 0)
+
 try:
     #For dielectric class
     from plasmapy.physics import (
@@ -61,10 +79,23 @@ try:
         chemical_potential,
     )
 
+    from plasmapy.mathematics import (
+        plasma_dispersion_func,
+        plasma_dispersion_func_deriv,
+        Fermi_integral
+    )
+
     #For relativity class
     from plasmapy.physics import Lorentz_factor
+    from plasmapy.physics import parameters
 except ImportError:
     from plasmapy.formulary import parameters
+
+    from plasmapy.formulary import (
+        plasma_dispersion_func,
+        plasma_dispersion_func_deriv,
+        Fermi_integral
+    )
 
     #For dielectric class
     from plasmapy.formulary import (
